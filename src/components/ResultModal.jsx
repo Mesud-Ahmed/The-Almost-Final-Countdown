aa455046ec1aa4bb88de32ed799e1939c0d5005d
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-21 10:00:38 +0300

    feat: Add dynamic score calculation based on remaining time and display messages accordingly.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,12 @@
 import { useRef, forwardRef, useImperativeHandle } from "react"
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({  targetTime, remainigTime, onReset }, ref) {
+    
     const dialog = useRef()
+    const userLost = remainigTime <= 0
+    const formatedTime = (remainigTime / 1000).toFixed(2)
+    const score = Math.round((1 - remainigTime / (targetTime * 1000)) * 100)
+
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -10,11 +15,13 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
         }
     })
     return (
-        <dialog ref={dialog} className="result-modal" >
-            <h2>You {result}</h2>
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime}</strong> second</p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formatedTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>close</button>
             </form>
         </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,19 @@
 import { useRef, useState } from "react"
 import ResultModal from "./ResultModal"
 export default function TimerChallenge({ title, targetTime }) {
-    const[timeRemaining,setTimeRemainig] = useState(targetTime * 1000)
+    const [timeRemaining, setTimeRemainig] = useState(targetTime * 1000)
 
     const timer = useRef()
     const dialog = useRef()
     const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000
 
-    if(timeRemaining <= 0 ){
+    if (timeRemaining <= 0) {
         clearInterval(timer.current)
-        setTimeRemainig(targetTime * 1000)
+
         dialog.current.open()
     }
     const handleStart = () => {
-       
+
         timer.current = setInterval(() => {
             setTimeRemainig(prevTime => prevTime - 10)
         }, 10)
@@ -22,13 +22,21 @@ export default function TimerChallenge({ title, targetTime }) {
         dialog.current.open()
         clearInterval(timer.current)
     }
+    const handleReset = () => {
+        setTimeRemainig(targetTime * 1000)
+    }
     return (
 
         <>
-            <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+            <ResultModal
+                ref={dialog}
+                targetTime={targetTime}
+                remainigTime={timeRemaining}
+                onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{title}</h2>
-                
+
                 <p className="challenge-time">
                     {targetTime} second{targetTime > 1 ? 's' : ''}
 
